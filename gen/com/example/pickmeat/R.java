f50214bf61d982636109ea2528dbc8dd452a52da
tharking <kamal.choudhary@gmail.com>
2014-06-07 18:13:53 +0530

    added random class files

--- gen/com/example/pickmeat/R.java
+++ gen/com/example/pickmeat/R.java
@@ -10,6 +10,19 @@ package com.example.pickmeat;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black_overlay=0x7f040000;
+        public static final int blue_actionbar=0x7f040001;
+        public static final int header_bg=0x7f040005;
+        public static final int header_fg=0x7f040006;
+        public static final int light_blue=0x7f040004;
+        public static final int menu_background=0x7f040002;
+        public static final int page_bg=0x7f040009;
+        public static final int page_fg=0x7f04000a;
+        public static final int ratings_bg=0x7f040007;
+        public static final int ratings_fg=0x7f040008;
+        public static final int white=0x7f040003;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +30,47 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LiftExpandableList=0x7f090000;
+        public static final int acceptView=0x7f090008;
+        public static final int action_settings=0x7f090010;
+        public static final int addRequest=0x7f090006;
+        public static final int editTextFrom=0x7f090002;
+        public static final int editTextLiftee=0x7f090001;
+        public static final int editTextTime=0x7f090004;
+        public static final int editTextTo=0x7f090003;
+        public static final int liftMidLayout=0x7f090009;
+        public static final int location=0x7f09000f;
+        public static final int recipeClockLayout=0x7f09000d;
+        public static final int saveButton=0x7f090005;
+        public static final int txtFrom=0x7f09000b;
+        public static final int txtLiftee=0x7f09000a;
+        public static final int txtTime=0x7f09000e;
+        public static final int txtTo=0x7f09000c;
+        public static final int viewPager=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int new_lift_request=0x7f030001;
+        public static final int widget_add_lift_button=0x7f030002;
+        public static final int widget_lift_item=0x7f030003;
+        public static final int widget_lift_item_accept=0x7f030004;
+        public static final int widget_lift_item_one=0x7f030005;
+        public static final int widget_location=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -59,10 +94,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
